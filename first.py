a1ecdc1ee6dc20fa9501d782f6a8519e07e6cb1b
IbneNisar <munib_ahmed@yahoo.com>
2020-04-25 21:09:12 -0500

    Update first.py

--- first.py
+++ first.py
@@ -62,7 +62,7 @@ while (count>0):
 
 
 #------------------------------Snippet 6----------------------------------
-#Following snippet asks for a number and prints multiplications facts for the number from upto 12
+#Following snippet asks for a number and prints multiplications facts for the number from 1 upto 12
 number = int(input(" Let's create multiplication table. Enter a number from 2 to 10 "))
 if number<2:
     print(" You entered a number less than 2. Good bye! ")
